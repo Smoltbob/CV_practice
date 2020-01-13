cac3cf77dd16a0271e3f281574179c6a624849f2
Jules Simon <jusimc@m3206-06.rdgi.polymtl.ca>
2020-01-13 14:08:03 -0500

    joute perceptron

---
+++ perceptron.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+"""
+Perceptron classifier.
+Parameters
+------------
+Learning rate (between 0.0 and 1.0)
+n_iter : int
+Passes over the training dataset.
+Attributes
+-----------
+w_ : 1d-array
+Weights after fitting.
+errors_ : list
+Number of misclassifications (updates) in each epoch.
+"""
+class Perceptron(object):
+    """
+    eta : float [0;1], learning rate
+    """
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    """
+    Fit training data.
+    Parameters
+    ----------
+    X : {array-like}, shape = [n_examples, n_features]
+    Training vectors, where n_examples is the number of
+    examples and n_features is the number of features.
+    y : array-like, shape = [n_examples]
+    Target values.
+    Returns
+    -------
+    self : object
+    """
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.errors_ = []
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+        """
+        Calculate net input
+        """
+        def net_input(self, X):
+            return np.dot(X, self.w_[1:]) + self.w_[0]
+
+        """
+        Return class label after unit step
+        """
+        def predict(self, X):
+            return np.where(self.net_input(X) >= 0.0, 1, -1)
